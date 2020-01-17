029c8d92b5ac6b75ffa4b7d48742bfb48d9be793
vicky1492 <59618881+vicky1492@users.noreply.github.com>
2020-01-17 13:22:02 +0530

    Update newvm.ps1

--- newvm.ps1
+++ newvm.ps1
@@ -1,1 +1,1 @@
-New-AzVm
+New-AzResourceGroupDeployment -ResourceGroupName Test -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-windows/azuredeploy.json
